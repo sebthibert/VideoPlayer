80430e377dad9111c0c41517b02cc20fee3c2cee
Sebastien Thibert <sebastien.thibert@marks-and-spencer.com>
2022-10-26 11:31:12 +0100

    Initial Commit

---
+++ VideoPlayer/VideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerApp.swift
+//  VideoPlayer
+//
+//  Created by Sebastien Thibert on 26/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
